0d1760fe7213999b7ac26f6c5b792147d83b4228
Edward Haddican <edwardhaddican@Edwards-MacBook-Pro.local>
2021-06-22 12:58:10 -0400

    second module finished

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
-const PALETTE = [
-  'red',
-  'blue',
-  'green',
-  'violet',
-  'black',
-  'turquoise', 
-  'pink',
-  'gold',
-]
 
-for (let index = 0; index < PALETTE.length; index = index + 1) {
-  // access the color
-  const nextColor = PALETTE[index]
-  // rest of code below
-  const newButton = $(`<button></button>`)
-  newButton.css('background-color', nextColor)
-  $('.palette').append(newButton)
+function makePalette(){
+  const PALETTE = [
+    'red',
+    'blue',
+    'green',
+    'violet',
+    'black',
+    'turquoise', 
+    'pink',
+    'gold',
+  ]
+  
+  for (let index = 0; index < PALETTE.length; index = index + 1) {
+    // access the color
+    const nextColor = PALETTE[index]
+    // rest of code below
+    const newButton = $(`<button></button>`)
+    newButton.css('background-color', nextColor)
+    $('.palette').append(newButton)
+  }
+  $('.palette button').first().addClass('active');
 }
 
+makePalette()
 
-for(let i = 0; i < 64; i++){
-  const newCell = $('<div></div>').addClass('cell')
-  $('.grid').append(newCell)
+
+function makeGrid(){
+  for(let i = 0; i < 64; i++){
+    const newCell = $('<div></div>').addClass('cell')
+    $('.grid').append(newCell)
+  }
 }
+
+makeGrid()
+
+function onPaletteClick(){
+  const currentColor = $(this)
+  $('.palette button').removeClass()
+  currentColor.addClass('active')
+}
+
+$('.palette button').click(onPaletteClick);
+
+function onGridClick(){
+    const currentColor = $('.palette .active').css('background-color')
+    const currentCell = $(this)
+    currentCell.css('backgroundColor', currentColor)
+}
+
+$('.grid .cell').click(onGridClick)
